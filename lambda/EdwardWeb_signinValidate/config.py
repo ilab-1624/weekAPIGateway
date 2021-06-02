97e86d24f1585c3755761329e2dd431f289c1a49
BananaDuncan <b10121014@gmail.com>
2021-06-02 22:19:59 +0800

    Update config.py

--- lambda/EdwardWeb_signinValidate/config.py
+++ lambda/EdwardWeb_signinValidate/config.py
@@ -3,7 +3,7 @@ aws_secret_access_key = 'aws secert key'
 region_name = 'your region'
 sourceBucketName = 'ai-big-data-system-20210523'
 
-collectionId = "API_Gateway_Class"
+collectionId = "your aws rekognition collectionId"
 
 USER_POOL_ID = 'your userpool id'
 
